aa19cb2f87e8ff6c48a4a8d97b4c56c7e62413f4
ydam113 <dydeka113@gmail.com>
2025-04-04 18:08:58 +0900

    2nd

--- api/login.js
+++ api/login.js
@@ -2,13 +2,13 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body
 
-    if (username && password) {
-      // 인증 로직은 여기에 작성
+    // 간단한 로그인 검사 로직
+    if (username !== '' && password !== '') {
       res.status(200).json({ message: '로그인 성공' })
     } else {
-      res.status(400).json({ message: '잘못된 입력' })
+      res.status(401).json({ message: '아이디 또는 비밀번호가 틀렸습니다.' })
     }
   } else {
-    res.status(405).json({ message: '허용되지 않은 메서드' })
+    res.status(405).json({ message: '허용되지 않은 메서드입니다.' })
   }
 }
